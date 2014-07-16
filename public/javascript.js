76a076a4fe8f64b0e17c3a8b2a038813fca15006
Rui Nakata <rnakata@colgate.edu>
2014-07-16 09:13:01 -0500

    refactored bugs

--- public/javascript.js
+++ public/javascript.js
@@ -1,49 +1,67 @@
 
-(function(){
+var Module = (function(){
 
-
+// this part is not accessible from console bc wrapped in anonymous fxn
 var counter = 0;
 var score = 0;
 var	question = quizzes[counter]["q"];
 var	answer = quizzes[counter]["answer"];
 var	exp = quizzes[counter]["exp"];
 
-$(function() {
+//this part is accessible from console because of this
+
+	var incrementscore = function(){
+		score += 1;
+	};
+	var incrementcounter = function(){
+		counter += 1;
+	};
+	var appendanswerexp = function(){
+		$("#quizbox").append("answer: " + answer + "<br>");
+		$("#quizbox").append(exp);
+	};
+	var hidetrue = function(){
+  	$("#true").css("visibility", "hidden")
+	};
+	var hidefalse = function(){
+  	$("#false").css("visibility", "hidden")
+	};
+
 	$("#quizbox").html("<h3>" + question + "</h3>");
 
 // when they press true//////////////////////////
 	$("#true").on('click', function(){
+		hidefalse();
+		hidetrue();
 		if (answer == true) {
 			alert("correct!");
-			$("#quizbox").append("answer: " + answer + "<br>");
-			$("#quizbox").append(exp);
-			score += 1;
+			appendanswerexp();
+			incrementscore();
 		}
 		else {
 			alert("wrong :/");
-			$("#quizbox").append("answer: " + answer + "<br>");
-			$("#quizbox").append(exp);
+			appendanswerexp();
 		}
 	});
 
 // when they press false /////////////////////////
 	$("#false").on('click', function(){
+		hidefalse();
+		hidetrue();
 		if (answer == false) {
 			alert("correct!");
-			$("#quizbox").append("answer: " + answer + "<br>");
-			$("#quizbox").append(exp);
-			score += 1;
+			appendanswerexp();
+			incrementscore();
 		}
 		else {
 			alert("wrong :/");
-			$("#quizbox").append("answer: " + answer + "<br>");
-			$("#quizbox").append(exp);
+			appendanswerexp();
 		}
 	});
 
 // when they press the next question /////////////
 	$("#next").on('click', function(){
-		counter += 1;
+		incrementcounter();
 		question = quizzes[counter]["q"]
 		answer = quizzes[counter]["answer"]
 		exp = quizzes[counter]["exp"]
@@ -61,13 +79,18 @@ $("#result").on('click', function(){
 });
 
 /////////////////////////////////////////////////
-});
 
-
+return {
+	incrementscore: incrementscore,
+	incrementcounter: incrementcounter,
+	appendanswerexp: appendanswerexp,
+	hidetrue: hidetrue,
+	hidefalse: hidefalse
+}
 
 })();
 
 
 
 
-
+// accessible thru Module.incrementscore
